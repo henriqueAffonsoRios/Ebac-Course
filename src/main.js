fbc48dadc3b6136c10ebe0d33032d2c9d17cd013
henriqueAffonsoRios <henriqueaffonsorios@gmail.com>
2024-05-10 13:17:03 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+function multiplicacao(A, B) {
+    return A * B;
+}
+function saudacao(nome) {
+    return "Ol\u00E1 ".concat(nome);
+}
+console.log(multiplicacao(2, 5));
+console.log(saudacao('joao'));
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function multiplicacao (A: number, B: number): number {
+  return A * B;
+}
+
+function saudacao (nome: string): string {
+  return `Olá ${nome}`;
+}
+
+console.log(multiplicacao(2, 5));
+console.log(saudacao('joao'));
